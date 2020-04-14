601dca9a6743c173c8bbf07334501e394968b7b4
Leuk Travis Mangila <50103787+DrSack@users.noreply.github.com>
2020-04-14 17:50:05 +1000

    test

---
+++ src/Styling/Styling.js
@@ -0,0 +1,124 @@
+let style = {
+    float: "right",
+    color: "white",
+    height: "90px",
+    width: "75%",
+    fontSize: "2.3vh",
+  }
+  
+  let style2 = {
+    height: "90px",
+    width:  "100%",
+    margin: "0px",
+    padding: "0px",
+    fontFamily:"Titillium Web",
+  }
+  
+  let style3 = {
+    float: "left",
+    display: "flex",
+    height: "90px",
+    marginLeft: '3vh',
+    whiteSpace: "nowrap",
+    fontSize: "2vh",
+    lineHeight: "90px",
+  }
+  
+  let banner = {
+    display: "flex",
+    testAlign: "center",
+    whiteSpace: "nowrap",
+    float: "left",
+    height: "90px",
+    lineHeight: "90px",
+    textAlign: "left",
+    width: "16%",
+    margin: "0 auto",
+  }
+
+  let middleLeft ={
+    paddingTop: "50px",
+    float: "left",
+    width: "50%"
+  }
+
+  let middleRight ={
+    float: "right",
+    width: "50%",
+    paddingTop: "50px"
+    }
+
+    let middleBottom ={
+      position:"absolute",
+      marginTop: "280px",
+      width:"100%",
+    }
+
+    let pStyle ={
+        padding: "0",
+        margin: "0",
+    }
+
+    let pStyleRight ={
+      padding: "0",
+      margin: "0",
+      testAlign: "right"
+  }
+
+    let buttonStyle ={
+        padding: "10px",
+        margin: "30px",
+    }
+
+let middle={
+    color: "White",
+    backgroundColor: "#8ABAD3FF",
+    height: "100%",
+    width: "100%"
+}
+
+let middleBoom1={
+  display: "inline-block",
+  float: "left",
+  width: "33%"
+}
+
+let middleBoom2={
+  display: "inline-block",
+  float: "left",
+  width: "33%"
+}
+
+let middleBoom3={
+  display: "inline-block",
+  float: "left",
+  width: "33%"
+}
+
+let middleBoom4={
+  fontSize: "15px",
+}
+
+let DataContainer={
+  display: "inline-block",
+  fontSize: "15px",
+  float: "left",
+  width: "25%",
+}
+
+let DataContainer2={
+  clear: "left",
+  width: "100%"
+}
+
+let DataContainer3={
+  display: "flex",
+  fontSize: "15px",
+  float: "left",
+  width: "48.9%",
+  border: "1px solid black",
+  marginLeft: "1.5vh",
+  height: "100px"
+}
+
+  export {style,style2,style3,banner,middleLeft,middleRight,middle,pStyle,middleBottom,buttonStyle,middleBoom1,middleBoom2,middleBoom3,middleBoom4,DataContainer,pStyleRight,DataContainer2,DataContainer3};
